22b82c341a3c4e40baac9e124f8182d491ccbffc
Naveenkumar M <naveenpyit@gmail.com>
2025-07-05 13:03:21 +0530

    popups.jsx

--- popups.jsx
+++ popups.jsx
@@ -3,7 +3,36 @@ import './popup.css';
 export default function Popups({action}){
     return(
         <>
-            <div className={action}>hi bro</div>
+            <div className={action}>
+                <div className='topSection'>
+                    <div style={{height: '21%'}}>
+                        <img className='profileSrc' src="src/assets/added image.jpg" alt="" />
+                    </div>
+                    <div className='linkWords'>
+                        Profile
+                    </div>
+                    <div className='lineEnd'></div>
+                    <div className='linkWords'>
+                        Security management
+                    </div>
+                    <div className='lineEnd'></div>
+                    <div className='linkWords'>
+                        Settings
+                    </div>
+                    <div className='lineEnd'></div>
+                    <div className='linkWords'>
+                        Privacy Policy
+                    </div>
+                </div>
+                <div className='bottomSection'>
+                    <div>
+                        <button className='anotherLog'>Login Another Account</button>
+                    </div>
+                    <div style={{height: '32px'}}>
+                        <button className='logOut'>Logout</button>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
